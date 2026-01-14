84e3c88a97d58bbe71ea20c0c033bf8db1d24752
ayushabhinandan269-png <ayushabhinandan269@gmail.com>
2026-01-14 12:32:00 +0530

    changes in app store

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "../features/cart/cartSlice";
-import productReducer from "../features/products/productSlice";
+import productReducer from "../products/productSlice";
 
 export const store = configureStore({
   reducer: {
