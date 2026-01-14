7df181dafe36a19937c659fef92604c94ce4692f
ayushabhinandan269-png <ayushabhinandan269@gmail.com>
2026-01-14 12:03:33 +0530

    feat: add cart quantity controls

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,5 +1,32 @@
-const Header = () => {
-  return <h2>Header</h2>;
+import { useDispatch } from "react-redux";
+import {
+  increaseQuantity,
+  decreaseQuantity,
+  removeFromCart,
+} from "../features/cart/cartSlice";
+
+const CartItem = ({ item }) => {
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <h4>{item.title}</h4>
+      <p>Quantity: {item.quantity}</p>
+
+      <button onClick={() => dispatch(increaseQuantity(item.id))}>
+        +
+      </button>
+
+      <button onClick={() => dispatch(decreaseQuantity(item.id))}>
+        -
+      </button>
+
+      <button onClick={() => dispatch(removeFromCart(item.id))}>
+        Remove
+      </button>
+    </div>
+  );
 };
 
-export default Header;
+export default CartItem;
+
