6561d9cd8967c70cb5be77f21b2f5caa349904ff
ayushabhinandan269-png <ayushabhinandan269@gmail.com>
2026-01-14 11:40:52 +0530

    feat: setup createBrowserRouter

---
+++ src/routes/router.jsx
@@ -0,0 +1,31 @@
+import { createBrowserRouter } from "react-router-dom";
+import { lazy } from "react";
+
+const ProductList = lazy(() => import("../components/ProductList"));
+const ProductDetail = lazy(() => import("../components/ProductDetail"));
+const Cart = lazy(() => import("../components/Cart"));
+const Checkout = lazy(() => import("../components/Checkout"));
+const NotFound = lazy(() => import("../components/NotFound"));
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <ProductList />,
+  },
+  {
+    path: "/product/:id",
+    element: <ProductDetail />,
+  },
+  {
+    path: "/cart",
+    element: <Cart />,
+  },
+  {
+    path: "/checkout",
+    element: <Checkout />,
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+]);
