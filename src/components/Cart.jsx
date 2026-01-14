ca22329bd40ea3be70a9b813f168d2d334a68e53
ayushabhinandan269-png <ayushabhinandan269@gmail.com>
2026-01-14 12:02:56 +0530

    feat: render cart items list

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,5 +1,26 @@
-const Header = () => {
-  return <h2>Header</h2>;
+import { useSelector } from "react-redux";
+import { selectCartItems } from "../features/cart/cartSelectors";
+import CartItem from "./CartItem";
+import { Link } from "react-router-dom";
+
+const Cart = () => {
+  const items = useSelector(selectCartItems);
+
+  if (items.length === 0) {
+    return <h3>Your cart is empty</h3>;
+  }
+
+  return (
+    <div>
+      <h2>Your Cart</h2>
+
+      {items.map((item) => (
+        <CartItem key={item.id} item={item} />
+      ))}
+
+      <Link to="/checkout">Proceed to Checkout</Link>
+    </div>
+  );
 };
 
-export default Header;
+export default Cart;
