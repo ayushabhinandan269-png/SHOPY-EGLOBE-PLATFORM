2936e1ab185ec61206cf6f9983780eead845c8eb
ayushabhinandan269-png <ayushabhinandan269@gmail.com>
2026-01-14 11:46:56 +0530

    feat: fetch products in product list

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,5 +1,21 @@
-const Header = () => {
-  return <h2>Header</h2>;
+import useFetchProducts from "../hooks/useFetchProducts";
+
+const ProductList = () => {
+  const { products, loading, error } = useFetchProducts();
+
+  if (loading) return <p>Loading products...</p>;
+  if (error) return <p>Error: {error}</p>;
+
+  return (
+    <div>
+      <h2>Products</h2>
+      {products.map((product) => (
+        <div key={product.id}>
+          <p>{product.title}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
-export default Header;
+export default ProductList;
