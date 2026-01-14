b583a1ddce343e4f761e7a060f6f73ffa5cf5fe0
ayushabhinandan269-png <ayushabhinandan269@gmail.com>
2026-01-14 12:38:09 +0530

    fix: correct product slice import path

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "../features/cart/cartSlice";
-import productReducer from "../products/productSlice";
+import productReducer from "../product/productSlice";
 
 export const store = configureStore({
   reducer: {
