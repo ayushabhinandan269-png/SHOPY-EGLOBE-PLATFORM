90d34267a4ee21c5a8fcd8f2a8d0fd45a9d51eaf
ayushabhinandan269-png <ayushabhinandan269@gmail.com>
2026-01-14 11:41:34 +0530

    feat: integrate router with suspense

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import { RouterProvider } from "react-router-dom";
+import { Suspense } from "react";
+import { router } from "./routes/router";
+
 const App = () => {
-  return <h1>ShoppyGlobe</h1>;
+  return (
+    <Suspense fallback={<p>Loading...</p>}>
+      <RouterProvider router={router} />
+    </Suspense>
+  );
 };
 
 export default App;
